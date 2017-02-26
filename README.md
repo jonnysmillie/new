c3f4b038b1ab053c84727632e064e6a6b2223394
jcsmillie <jsmillie.c@gmail.com>
2017-02-26 23:00:44 +0000

    got pug template working with css, added Twit modile and setup config file

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 This is the Readme file for a Node.js based Twitter client.
 
 Any relevant information will be added here.
+
+1. This client application requires a config.js file at root level. This file should look like this:
+
+	var Twit = require('twit')
+
+var T = new Twit({
+    consumer_key:         'xxx'
+  , consumer_secret:      'xxx'
+  , access_token:         'xxx'
+  , access_token_secret:  'xxx'
+})
--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
 var express = require('express')
 var app = express()
+//
+var Twit = require('twit')
+var config = require('./config.js');
+//
+const pug = require('pug');
+
+app.set('view engine', 'pug');
+//set where to look for templates
+app.set('views', __dirname + '/templates');
+
+
+
+
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send(pug.renderFile('../template.pug'))
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('App listening on port 3000!')
 })
